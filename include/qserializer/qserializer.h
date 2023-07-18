2c3a95d9636357dea417eded79644d28a8107838
black-desk <me@black-desk.cn>
2023-07-18 10:32:12 +0800

    chore: fix typo
    Signed-off-by: black-desk <me@black-desk.cn>

--- include/qserializer/qserializer.h
+++ include/qserializer/qserializer.h
@@ -154,18 +154,18 @@ QSerializer<T>::QVariantMapToPStrMap(QVariantMap map)
 }
 
 #if QT_VERSION <= QT_VERSION_CHECK(6, 2, 0)
-#define QSERIALIZER_DECLARE_SHAREPOINTER_METATYPE(x) \
+#define QSERIALIZER_DECLARE_SHAREDPOINTER_METATYPE(x) \
         Q_DECLARE_METATYPE(QSharedPointer<x>);
 #else
-#define QSERIALIZER_DECLARE_SHAREPOINTER_METATYPE(x)
+#define QSERIALIZER_DECLARE_SHAREDPOINTER_METATYPE(x)
 #endif
 
-#define QSERIALIZER_DECLARE(T)                        \
-        QSERIALIZER_DECLARE_SHAREPOINTER_METATYPE(T); \
-        namespace QSerializerPrivateNamespace##T      \
-        {                                             \
-                char init();                          \
-                static char _ = init();               \
+#define QSERIALIZER_DECLARE(T)                         \
+        QSERIALIZER_DECLARE_SHAREDPOINTER_METATYPE(T); \
+        namespace QSerializerPrivateNamespace##T       \
+        {                                              \
+                char init();                           \
+                static char _ = init();                \
         };
 
 #define QSERIALIZER_IMPL(T, ...)                                      \
